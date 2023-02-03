c06ecf5400ad3b07bf7c792b421a90360b4efc8a
DLong <daveallenlong@gmail.com>
2023-02-03 11:20:18 +0900

    Updated - nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
 	return (
 		<div className="navbar">
 			<h1 className="nav-title">Formula 1 Champions</h1>
 			<div className="links">
-				<a href="#">Drivers</a>
-				<a href="#">Constructors</a>
+				<Link to="/">Drivers</Link>
+				<Link to="/constructors">Constructors</Link>
 			</div>
 		</div>
 	);
