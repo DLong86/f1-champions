abe5bd1aa1805c055b70634dfbf57f83137be936
DLong <daveallenlong@gmail.com>
2023-02-03 10:34:09 +0900

    Added - ConstructorList

---
+++ src/pages/ConstructorList.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+export default function ConstructorList() {
+	const [constructors, setConstructors] = useState([]);
+	// const [selectedConstructors, setSelectedConstructors] =
+	// 	useState(constructors);
+	const url = "http://localhost:8080/constructors";
+
+	useEffect(() => {
+		fetch(url)
+			.then((response) => response.json())
+			.then((json) => setConstructors(json));
+	}, [url]);
+
+	console.log(constructors);
+
+	return (
+		<div>
+			<h1>World Constructor Champions List</h1>
+			{/* BELOW is the dropdown which should be its own component */}
+			<div>
+				<select>
+					<option value="select">select decade</option>
+					<option value="2020">2020s</option>
+					<option value="2010">2010s</option>
+					<option value="2000">2000s</option>
+					<option value="1990">1990s</option>
+					<option value="1980">1980s</option>
+					<option value="1970">1970s</option>
+					<option value="1960">1960s</option>
+					<option value="1950">1950s</option>
+				</select>
+			</div>
+
+			{/* <Dropdown /> */}
+			{constructors.map((constructor) => (
+				<li key={constructor.id} className="driver-list">
+					<h4>{constructor.year}</h4>
+					<h3>{constructor.chassis}</h3>
+					<p className="text-slate-400">{constructor.engine}</p>
+				</li>
+			))}
+		</div>
+	);
+}
