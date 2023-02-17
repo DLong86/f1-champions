b816a71924c1800d9654e467a2bb5e8d1bf9df7c
DLong <daveallenlong@gmail.com>
2023-02-17 11:14:04 +0900

    Added - constructor drivers to List

--- src/components/lists/List.js
+++ src/components/lists/List.js
@@ -8,7 +8,17 @@ export default function List({ info }) {
 				<h3>{info.name || info.entrant}</h3>
 				<p className="text-slate-400">{info.Nationality || info.engine}</p>
 				<h5>{info.team}</h5>
-				<p>{info.drivers}</p>
+				{info.driver1 && (
+					<div>
+						<small>{info.driver1}</small>
+						<br />
+						<small>{info.driver2}</small>
+						<br />
+						<small>{info.driver3}</small>
+						<br />
+						<small>{info.driver4}</small>
+					</div>
+				)}
 			</li>
 		</ul>
 	);
